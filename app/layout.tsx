9313e4f5a30f67725e3e5294ea7e247b99ba74b4
Craig Blagg <cblagg@squarespace.com>
2024-10-24 10:31:27 -0400

    Cast cart type

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Navbar } from 'components/layout/navbar';
 import { WelcomeToast } from 'components/welcome-toast';
 import { GeistSans } from 'geist/font/sans';
 import { getCart } from 'lib/squarespace';
+import { Cart } from 'lib/squarespace/types';
 import { ensureStartsWith } from 'lib/utils';
 import { cookies } from 'next/headers';
 import { ReactNode } from 'react';
@@ -43,7 +44,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
   return (
     <html lang="en" className={GeistSans.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
-        <CartProvider initialCart={cart}>
+        <CartProvider initialCart={cart as Cart}>
           <Navbar />
           <main>
             {children}
